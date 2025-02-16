4acf2a1b9c0ea301a68fef68aed11642c8409096
Anwar Work <Anwaralbana11111@gmail.com>
2025-02-16 22:36:34 +0300

    this First commet from New Project to BankProject

--- BankProject_UsingCppWithOpp.cpp
+++ BankProject_UsingCppWithOpp.cpp
@@ -1,9 +1,11 @@
-
+//Anwar Albnna 
+// From Programming Advices
 #include <iostream>
 #include "clsMainScreen.h"
 using namespace std;
 int main()
 {
+    
     clsMainScreen::ShowMainMenue();
     return 0;
 }
